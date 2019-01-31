7729faff9c5fda8d2b67c47208602bbdb4ee2bdc
Roman Metlinskyi <gmail@metlinskyi.com>
2019-01-31 23:36:05 +0200

    fix bootstrap

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,34 +1,25 @@
 #!/usr/bin/env sh
 
-
 git config --global user.email "gmail@metlinskyi.com"
 git config --global user.name "Roman Metlinskyi"
 
 app='/usr/share/app'
-if find "$app" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
-    echo "$app is exists"
-else
-    mkdir $app
-fi
 
 target='angular'
-if find "$app/$target" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
-    echo "$app/$target is exists"
-else
+if [ `ls -A "$app/$target/" | wc -m` == "0" ]; then
+    echo "$app/$target/ empty"
     cd "$app/"
     ng new $target
 fi
 
 target='polymer'
-if find "$app/$target" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
-    echo "$app/$target is exists"
-else
+if [ `ls -A "$app/$target/" | wc -m` == "0" ]; then
+    echo "$app/$target/ empty"
     cd "$app/"
     git clone https://github.com/PolymerLabs/start-polymer3.git $target
     cd "$target"
     npm install
 fi
-
 
 chmod -R 777 /usr/share/app
 chmod -R 777 /usr/share/nginx/html
